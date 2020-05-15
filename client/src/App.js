37947de2fb8a1edce84384298e0a96960d37d497
VK <VictoriaToria@Victorias-Mac.box>
2020-05-15 15:47:53 +0200

    map fully functioning

--- client/src/App.js
+++ client/src/App.js
@@ -1,95 +1,113 @@
 import React from "react";
 // import MapView from "./components/MapView";
-import ResponseMap from "./components/ResponseMap";
+// import ResponseMap from "./components/ResponseMap";
 // import TripReview from "./components/TripReview";
-import FilterPanel from "./components/FilterPanel";
-// import ReactMapGL from "react-map-gl";
-// import MapboxDirections from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions";
-// import mapboxgl from "mapbox-gl";
+// import FilterPanel from "./components/FilterPanel";
+import ReactMapGL from "react-map-gl";
+import MapboxDirections from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions";
+import mapboxgl from "mapbox-gl";
 // import MapBoxGLDraw from '@mapbox/mapbox-gl-draw';
 import "./App.css";
-import { Route } from "react-router-dom";
-import Signup from "./components/Signup";
+// import { Route } from "react-router-dom";
+// import Signup from "./components/Signup";
 import Navbar from "./components/Navbar";
-import Login from "./components/Login";
+// import Login from "./components/Login";
 
 // mapboxgl.accessToken =
 //   "pk.eyJ1IjoidmljdG9yaWF0b3JpYSIsImEiOiJja2EzbHVrMnowMzBzM2tyd2VsNnI2YnFiIn0.rZpPyrN5hdNxsnVtAWWCOQ";
 
-
-
 class App extends React.Component {
   state = {
     user: this.props.user,
-    title: '',
+    showButton: false,
     origin: [],
-    origin_name: '',
+    origin_name: "",
     destination: [],
-    destination_name: '',
+    destination_name: "",
     map: null,
     draw: null,
-    lng: 13.4050,
-    lat: 52.5200,
+    lat: 52.52,
+    lng: 13.405,
     zoom: 13,
-    uuid: '',
-    duration: '',
-    distance: '',
+    uuid: "",
+    duration: "",
+    distance: "",
     coordinates: [],
     waypoints: [],
     reviewTrip: false,
-    user: '',
+    user: "",
   };
 
- 
+  componentDidMount = () => {
+    console.log(this.state.lat, this.state.lng);
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-79.4512, 43.6568],
+      zoom: 13,
+    });
 
-//   componentDidMount = () => {
-//     console.log(this.state.lat, this.state.lng);
-//     const map = new mapboxgl.Map({
-//       container: this.mapContainer,
-//       style: "mapbox://styles/mapbox/streets-v11",
-//       center: [this.state.lng, this.state.lat],
-//       zoom: this.state.zoom,
-//       duration: this.state.duration,
-//       distance: this.state.distance,
-//     });
+    // map.on("route", () => {
+    //   console.log(this.state);
+    //   this.setState({
+    //     lng: map.getCenter().lng.toFixed(4),
+    //     lat: map.getCenter().lat.toFixed(4),
+    //     zoom: map.getZoom().toFixed(2),
+    //   });
+    // });
+    map.addControl(
+      new MapboxDirections({
+        accessToken: mapboxgl.accessToken,
+        unit: "metric",
+        profile: "mapbox/cycling",
+      }),
 
-//     const canvas = map.getCanvasContainer();
+      "top-left"
+    );
+    map.addControl(new mapboxgl.NavigationControl());
 
-//     map.on("route", () => {
-//       console.log(this.state);
-//       this.setState({
-//         lng: map.getCenter().lng.toFixed(4),
-//         lat: map.getCenter().lat.toFixed(4),
-//         zoom: map.getZoom().toFixed(2),
-//       });
-//     });
-//     map.addControl(
-//       new MapboxDirections({
-//         accessToken: mapboxgl.accessToken,
-//         unit: "metric",
-//         profile: "mapbox/cycling",
-//       }),
+    //   map.getDirections([
+    //     origin,
+    //     destination
+    //   ], {
+    //     profile: 'mapbox.driving',
+    //     instructions: 'html',
+    //     alternatives: false,
+    //     geometry: 'geojson'
+    // }).then(function(results) {
+    //   let { origin, destination, routes } = results
+    //   this.setState({
+    //     directions: routes[0].geometry.coordinates
+    //   })
+    // })
+    setInterval(() => {
+      const routeEle = document.querySelector(
+        ".mapbox-directions-route-summary > h1"
+      );
+      if (routeEle && !this.state.showButton) {
+        this.setState({
+          showButton: true,
+        });
+      } else if (!routeEle && this.state.showButton) {
+        this.setState({
+          showButton: false,
+        });
+      }
+    }, 500);
+  };
+  getRoute = () => {
+    console.log(
+      "distance: ",
+      document.querySelector(".mapbox-directions-route-summary > h1").innerHTML
+    );
 
-//       "top-left"
-//     );
-//     map.addControl(new mapboxgl.NavigationControl());
-  
-// //   map.getDirections([
-// //     origin,
-// //     destination
-// //   ], {
-// //     profile: 'mapbox.driving',
-// //     instructions: 'html',
-// //     alternatives: false,
-// //     geometry: 'geojson'
-// // }).then(function(results) {
-// //   let { origin, destination, routes } = results
-// //   this.setState({
-// //     directions: routes[0].geometry.coordinates
-// //   })
-// // })
-// };
-    
+    const fromToEle = document.getElementsByClassName("mapboxgl-ctrl-geocoder");
+
+    console.log("from: ", fromToEle[0].querySelector("input").value);
+
+    console.log("to: ", fromToEle[1].querySelector("input").value);
+  };
+
   setUser = (user) => {
     this.setState({
       user: user,
@@ -99,36 +117,39 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-      {/* <div><MapView/></div> */}
-      <ResponseMap/>
-      <FilterPanel/>
-      
-         <div>
-          <div>
-            <div>
-              Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom:{" "}
-              {this.state.zoom}
-            </div>
-          </div>
-       
-       
-       {/* <ReactMapGL
-          {...this.state.viewport}
-          onViewportChange={(viewport) => this.setState(viewport)}
-          mapboxApiAccessToken="pk.eyJ1IjoidmljdG9yaWF0b3JpYSIsImEiOiJja2EzbHVrMnowMzBzM2tyd2VsNnI2YnFiIn0.rZpPyrN5hdNxsnVtAWWCOQ"
-        ></ReactMapGL>  */}
         <Navbar user={this.state.user} setUser={this.setUser} />
-        <Route
-          exact
-          path="/signup"
-          render={(props) => <Signup setUser={this.setUser} {...props} />}
-        />
-        <Route
-          exact
-          path="/login"
-          render={(props) => <Login setUser={this.setUser} {...props} />}
-        />
-      </div>
+        {/* <div><MapView/></div> */}
+        {/* <ResponseMap />
+        <FilterPanel /> */}
+        <div class="map">
+          <div
+            ref={(el) => (this.mapContainer = el)}
+            className="mapContainer"
+          />
+          <div>
+            <ReactMapGL
+              {...this.state.viewport}
+              onViewportChange={(viewport) => this.setState(viewport)}
+              mapboxApiAccessToken="pk.eyJ1IjoidmljdG9yaWF0b3JpYSIsImEiOiJja2EzbHVrMnowMzBzM2tyd2VsNnI2YnFiIn0.rZpPyrN5hdNxsnVtAWWCOQ"
+            ></ReactMapGL>
+          </div>
+          {this.state.showButton ? (
+            <button onClick={this.getRoute}>BUTTON</button>
+          ) : (
+            ""
+          )}
+
+          {/* <Route
+            exact
+            path="/signup"
+            render={(props) => <Signup setUser={this.setUser} {...props} />}
+          />
+          <Route
+            exact
+            path="/login"
+            render={(props) => <Login setUser={this.setUser} {...props} />}
+          /> */}
+        </div>
       </div>
     );
   }
