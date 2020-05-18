acbe2f091f2404f1bf79c01a55bea4f3fd9a2b4a
paulamoellendorf <61729055+paulamoellendorf@users.noreply.github.com>
2020-05-18 08:38:07 +0200

    small changes

--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 import RoutesList from './RoutesList';
-import Dashboard from './Dashboard';
 import Profilepage from './ProfilePage'
 
 
--- routes/route.js
+++ routes/route.js
@@ -28,21 +28,25 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   console.log('post route');
-  const { startpoint, endpoint, kilometer, co2emission, ownerId } = req.body;
+  const startpoint=req.body.startpoint;
+  const endpoint=req.body.endpoint;
+ const kilometer=req.body.kilometer;
+ const co2emission=req.body.co2emission;
+  const owner= req.user._id;
 
   Route.create({
     startpoint, 
     endpoint, 
     kilometer, 
     co2emission,
-    owner:ownerId
+    owner
   })
     .then(route => {
       return Route.findByIdAndUpdate(ownerId, {
         $push: { routes: route._id }
       }).then(() => {
         res.status(201).json({
-          message: `Task with id ${route._id} was successfully added to project with id ${ownerId}`
+          message: `Task with id ${route._id} was successfully added to project with id ${owner}`
         });
       });
     })
