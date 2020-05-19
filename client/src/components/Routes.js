affae30acdf71d4b6e02bbfbdae83a0fb3e46740
paulamoellendorf <61729055+paulamoellendorf@users.noreply.github.com>
2020-05-19 15:28:56 +0200

    tree logic

--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import axios from "axios";
+// import axios from "axios";
 
 import RoutesList from "./RoutesList";
 // import Dashboard from "./Dashboard";
