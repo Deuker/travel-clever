3a7155d019cd1098ff2dc91ea38e12a2d29021fe
VK <VictoriaToria@Victorias-Mac.box>
2020-05-19 20:45:05 +0200

    footer done

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -17,10 +17,9 @@ class ProfilePage extends Component {
     //startpoint: this.props.startpoint,
     // endpoint: this.props.endpoint,
     // kilometer: this.props.kilometer,
-    // 
+    //
     //showInfo: false,
     co2emission: "",
-   
   };
 
   componentDidMount() {
@@ -28,36 +27,39 @@ class ProfilePage extends Component {
   }
 
   refreshDasboardAfterSaving() {
-      axios
-        .get("api/routes")
-        .then((response) => {
-          let totalKilometer = 0;
-          let totalCo2Saved = 0;
-          let totalTreeCapacitySaved = 0;
-          
-            for (let i=0; i<response.data.length; i++){
-              let singleKilometer = parseFloat(response.data[i].kilometer.split("km"))
-              let singleCo2 = parseFloat(response.data[i].co2emission)
-              console.log(singleKilometer);
-              totalKilometer += singleKilometer;
-              totalCo2Saved += singleCo2;
-              totalTreeCapacitySaved = totalKilometer*203.182/1000
-              console.log(totalCo2Saved);
-            }
-          console.log(totalKilometer);
-          console.log(response.data);
-          console.log(response.data[0].kilometer);
+    axios
+      .get("api/routes")
+      .then((response) => {
+        let totalKilometer = 0;
+        let totalCo2Saved = 0;
+        let totalTreeCapacitySaved = 0;
 
-          this.setState({totalKilometer, totalCo2Saved, totalTreeCapacitySaved});
-        })
-        
+        for (let i = 0; i < response.data.length; i++) {
+          let singleKilometer = parseFloat(
+            response.data[i].kilometer.split("km")
+          );
+          let singleCo2 = parseFloat(response.data[i].co2emission);
+          console.log(singleKilometer);
+          totalKilometer += singleKilometer;
+          totalCo2Saved += singleCo2;
+          totalTreeCapacitySaved = (totalKilometer * 203.182) / 1000;
+          console.log(totalCo2Saved);
+        }
+        console.log(totalKilometer);
+        console.log(response.data);
+        console.log(response.data[0].kilometer);
 
-        .catch((error) => {
-          console.log(error);
+        this.setState({
+          totalKilometer,
+          totalCo2Saved,
+          totalTreeCapacitySaved,
         });
-      }
-    
+      })
 
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   // componentDidMount = () => {
   //   this.props.getData();
@@ -71,11 +73,9 @@ class ProfilePage extends Component {
       co2emission: parseFloat(
         (parseInt(nextProps.kilometer) * 203.182) / 1000
       ).toFixed(2),
-
       showInfo: true,
 
       showRouteInfo: nextProps.showRouteInfo,
-
     };
   }
 
@@ -84,14 +84,6 @@ class ProfilePage extends Component {
     const sliced = parseFloat(str.split("km"));
     console.log("Co2 logic", sliced);
   };
-
-  // routeInfo = (event) => {
-  //   if (this.state.startpoint && this.state.endpoint && this.state.kilometer) {
-  //     this.setState({ showRouteInfo: true });
-  //   } else {
-  //     this.setState({ showRouteInfo: false });
-  //   }
-  // };
 
   handleSubmit = (event) => {
     event.preventDefault();
@@ -111,31 +103,13 @@ class ProfilePage extends Component {
         this.refreshDasboardAfterSaving();
         console.log("CO2 Data:", this.state.co2emission);
         this.props.getData();
-        
-        // this.setState({
-        //   startpoint: "",
-        //   endpoint: "",
-        //   kilometer: "",
-        // });
+
         this.props.closeShowRouteInfo();
       })
       .catch((err) => {
         console.log(err);
       });
   };
-
-  // handleChange = (event) => {
-
-  //   //event.preventDefault()
-  //   const name = event.target.name;
-  //   const value = event.target.value;
-  //   console.log(event);
-  //   console.log(event.target.name);
-  //   console.log(event.target.value);
-  //   this.setState({
-  //     [name]: value,
-  //   });
-  // };
 
   render() {
     //console.log("Banana", this.state);
@@ -160,46 +134,23 @@ class ProfilePage extends Component {
             ""
           )}
         </div>
-
-        {/* <Form onSubmit={this.handleSubmit}>
-          <Form.Group>
-            <Form.Label htmlFor="startpoint">Startpoint: </Form.Label>
-            <Form.Control
-              type="text"
-              name="startpoint"
-              value={this.state.startpoint}
-              onChange={this.handleChange}
-              id="startpoint"
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label htmlFor="endpoint">Endpoint: </Form.Label>
-            <Form.Control
-              type="text"
-              name="endpoint"
-              value={this.state.endpoint}
-              onChange={this.handleChange}
-              id="endpoint"
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label htmlFor="kilometres">Kilometer: </Form.Label>
-            <Form.Control
-              type="text"
-              name="kilometer"
-              value={this.state.kilometer}
-              onChange={this.handleChange}
-              id="kilometer"
-            />
-          </Form.Group> */}
-        {/* <Button type="submit">Save</Button> */}
-        {/* </Form> */}
-        <h3>Here we will show the Dashboard</h3>
-        <p>Total kilometers rode is {this.state.totalKilometer}</p>
-        <p>{}</p>
-        <p>Total CO2 saved {this.state.totalCo2Saved}</p>
-        <p>tree capacity saved {this.state.totalTreeCapacitySaved}</p>
-
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            marginRight: "20%",
+            marginLeft: "5%",
+            borderStyle: "solid",
+          }}
+        >
+          <h3>Your travel Dashboard</h3>
+          <p>Total kilometers cycled {this.state.totalKilometer}</p>
+          <p>{}</p>
+          <p>Total CO2 saved: {this.state.totalCo2Saved} kg</p>
+          <p>
+            Amount of trees saved: {this.state.totalTreeCapacitySaved} trees
+          </p>
+        </div>
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import axios from "axios";
-
+// import StickyFooter from "react-sticky-footer";
 axios
   .get("/api/auth/loggedin")
   .then((response) => {
@@ -13,6 +13,36 @@ axios
     ReactDOM.render(
       <Router>
         <App user={response.data} />
+        {/* <StickyFooter
+          bottomThreshold={50}
+          normalStyles={{
+            backgroundColor: "#999999",
+            padding: "2rem",
+          }}
+          stickyStyles={{
+            backgroundColor: "rgba(255,255,255,.8)",
+            padding: "2rem",
+          }}
+        /> */}
+        <React.Fragment>
+          <footer>
+            <ul className="footer-options">
+              <li className="footer-link">
+                <a href="#" className="footer-linktext">
+                  Legal
+                </a>
+              </li>
+              <li className="footer-link">
+                <a href="#" className="footer-linktext">
+                  Contact Us
+                </a>
+              </li>
+            </ul>
+            <span>
+              © 2021 Developed by Travel-Clever Team. All Rights Reserved.
+            </span>
+          </footer>
+        </React.Fragment>
       </Router>,
       document.getElementById("root")
     );
