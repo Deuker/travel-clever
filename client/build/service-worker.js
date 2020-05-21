c3eceed4e225454b98a6f943833ec4df446e2aad
Lucas Deuker <lucas.deuker@gmail.com>
2020-05-21 10:49:39 +0200

    bike is back - main page wip - error fix heroku

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e559d2f2f4e0b558893a0ff875a8bd72.js"
+  "/precache-manifest.191285ef737f5a294bad059578f43874.js"
 );
 
 self.addEventListener('message', (event) => {
