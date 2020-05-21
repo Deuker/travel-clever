bdc82eca5a69463f12057020fc8f420dd44d3e9e
VK <VictoriaToria@Victorias-Mac.box>
2020-05-21 21:49:17 +0200

    picture LP

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -109,7 +109,6 @@ class ProfilePage extends Component {
         this.refreshDasboardAfterSaving();
         console.log("CO2 Data:", this.state.co2emission);
         this.props.getData();
-
         this.props.closeShowRouteInfo();
       })
       .catch((err) => {
